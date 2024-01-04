fb38170f13574ca0b1f3d8f58508e05f703e54fd
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-04 17:25:48 +0200

    add custom navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Carousel from "./components/carousel/Carousel"
 import Navigation from "./components/navigation/Navigation"
+
 import './css/embla.css';
+import styles from './main.module.css';
 
 function App() {
   return (
-    <>
+    <div className={styles['main-container']}>
       <Navigation/>
       <Carousel/>
-    </>
+    </div>
   )
 }
 
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,25 +1,35 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
 
 import styles from './navigation.module.css';
 
-
 const Navigation = () => {
     return (
-        <AppBar position="static">
-            <Toolbar className={styles['nav-container']}>
-                <Typography color='secondary' variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Villa Sani
-                </Typography>
+        <nav className={styles['navigation']}>
+            <div className={styles['left-section']}>
+                <h2>Villa Sani</h2>
+            </div>
 
-                <Button color='secondary'>Contact us</Button>
-                <Button color='secondary'>About us</Button>
-                <Button color='secondary'>Въпроси</Button>
+            <div className={styles['right-section']}>
+                <button>Button 1</button>
+                <button>Button 2</button>
+                <button>Button 3</button>
+            </div>
+        </nav>
+        // <AppBar position="static">
+        //     <Toolbar className={styles['nav-container']}>
+        //         <Typography color='secondary' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        //             Villa Sani
+        //         </Typography>
 
-            </Toolbar>
-        </AppBar>
+        //         <Button color='secondary'>Contact us</Button>
+        //         <Button color='secondary'>About us</Button>
+        //         <Button color='secondary'>Въпроси</Button>
+
+        //     </Toolbar>
+        // </AppBar>
     );
 }
 
