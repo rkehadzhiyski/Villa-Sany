9e5352d411fab13b7695caf3203bf7749df15d25
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-05 12:25:55 +0200

    style the navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,4 @@
-// import AppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 
 import styles from './navigation.module.css';
 
@@ -11,25 +8,19 @@ const Navigation = () => {
             <div className={styles['left-section']}>
                 <h2>Villa Sani</h2>
             </div>
-
+            
             <div className={styles['right-section']}>
-                <button>Button 1</button>
-                <button>Button 2</button>
-                <button>Button 3</button>
+                <Button sx={{
+                    color: '#9c5d30'
+                }} >Contact us</Button>
+                <Button sx={{
+                    color: '#9c5d30'
+                }} >About us</Button>
+                <Button sx={{
+                    color: '#9c5d30'
+                }} >Въпроси</Button>
             </div>
         </nav>
-        // <AppBar position="static">
-        //     <Toolbar className={styles['nav-container']}>
-        //         <Typography color='secondary' variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        //             Villa Sani
-        //         </Typography>
-
-        //         <Button color='secondary'>Contact us</Button>
-        //         <Button color='secondary'>About us</Button>
-        //         <Button color='secondary'>Въпроси</Button>
-
-        //     </Toolbar>
-        // </AppBar>
     );
 }
 
