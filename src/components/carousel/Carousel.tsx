78532bea3eb1fa44f65e7089cd5fcfad5b0d256f
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-05 15:06:30 +0200

    change pictures

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -10,9 +10,9 @@ const Carousel = () => {
     return (
         <div className="embla" ref={emblaRef}>
             <div className="embla__container">
-                <img className="embla__slide" src="https://www.rentavilla.com/Rentavilla/Home/image-thumb__1803__RAV_Home_Slide/Homepage%205.webp" alt="" />
-                <img className="embla__slide" src="https://www.thetimes.co.uk/travel/wp-content/uploads/sites/6/2021/06/crop_exclusive_luxury_villa_with_swimming_pool_credit_gettyimages-925052286.jpeg?w=2200&h=880&crop=1" alt="" />
-                <img className="embla__slide" src="https://www.rentavilla.com/_default_upload_bucket/image-thumb__181516__RAV_Home_Slide/Courchevel%20Luxury%20Chalet%20rental%202.webp" alt="" />
+                <img className="embla__slide" src="https://img.vila.bg/g/2260/182904.jpg" alt="" />
+                <img className="embla__slide" src="https://img.vila.bg/g/2260/182931.jpg" alt="" />
+                <img className="embla__slide" src="https://img.vila.bg/g/2260/182903.jpg" alt="" />
             </div>
         </div>
     );
