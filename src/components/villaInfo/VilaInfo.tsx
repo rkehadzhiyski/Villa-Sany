6de567da82268f92e18dc5f51092a602f1175e54
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-08 15:33:38 +0200

    fix map size

--- src/components/villaInfo/VilaInfo.tsx
+++ src/components/villaInfo/VilaInfo.tsx
@@ -62,7 +62,7 @@ const VillaInfo = () => {
             <div>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2946.1802925005545!2d27.722319489214566!3d42.402605692599316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a6c80e5682ec29%3A0x69a897279b09fcab!2sSani%20Holiday%20Village!5e0!3m2!1sbg!2sbg!4v1704714439177!5m2!1sbg!2sbg"
-                    width="2400"
+                    width="1900"
                     height="600"
                     style={{ border: '0' }}
                     allowFullScreen
